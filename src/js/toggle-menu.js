ea27ecce08ab9a07b119a8df91859f978b8c845f
djimenezchocron <djimenezweb@gmail.com>
2023-03-05 14:36:41 +0100

    Header arreglado

--- src/js/toggle-menu.js
+++ src/js/toggle-menu.js
@@ -1,7 +1,27 @@
+const toggleButton = document.getElementById('toggle-button');
 const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+
+const openMenu = () => {
+  toggleButton.dataset.status = 'open';
+  hamburger.classList.add('hamburger--close');
+  nav.classList.add('nav--show');
+  document.body.classList.add('overflow--hidden');
+};
+
+const closeMenu = () => {
+  toggleButton.dataset.status = 'closed';
+  hamburger.classList.remove('hamburger--close');
+  nav.classList.remove('nav--show');
+  document.body.classList.remove('overflow--hidden');
+};
 
 const toggleMenu = () => {
-  hamburger.classList.add('hamburger--close');
+  if (toggleButton.dataset.status === 'closed') {
+    openMenu();
+  } else if (toggleButton.dataset.status === 'open') {
+    closeMenu();
+  }
 };
 
 export { toggleMenu };

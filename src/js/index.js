78b59aeda78e17889b2d2e7e91c5c9eed17ec78d
djimenezchocron <djimenezweb@gmail.com>
2023-03-06 12:22:22 +0100

    project-grid

--- src/js/index.js
+++ src/js/index.js
@@ -7,19 +7,39 @@ import { toggleMenu } from './toggle-menu.js';
 
 const toggleButton = document.getElementById('toggle-button');
 const headerElement = document.getElementById('header');
-const headerHeight = -1 * headerElement.clientHeight + 'px';
+const headerHeight = headerElement.clientHeight;
 const homeElement = document.getElementById('home');
+const rootStyles = document.documentElement.style;
+const animatedEntries = document.querySelectorAll('.animate');
 
 const optionsHeaderScroll = {
   root: null,
   threshold: 0,
-  rootMargin: headerHeight
+  rootMargin: -1 * headerHeight + 'px'
 };
 
-toggleButton.addEventListener('click', toggleMenu);
+const optionsProjects = {
+  root: null,
+  threshold: 0.3,
+  rootMargin: '-100px'
+};
 
-const observer = new IntersectionObserver(entry => {
+const observerHeader = new IntersectionObserver(entry => {
   entry[0].isIntersecting ? headerElement.classList.remove('header--scrolled') : headerElement.classList.add('header--scrolled');
 }, optionsHeaderScroll);
 
-observer.observe(homeElement);
+const observerProjects = new IntersectionObserver((entries, observerProjects) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('fade-in');
+      observerProjects.unobserve(entry.target);
+    }
+  });
+}, optionsProjects);
+
+observerHeader.observe(homeElement);
+animatedEntries.forEach(entry => {
+  observerProjects.observe(entry);
+});
+rootStyles.setProperty('--scroll-padding', headerHeight + 18 + 'px');
+toggleButton.addEventListener('click', toggleMenu);
